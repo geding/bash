f68db415e7cc79d8f12b32f252a16e45cdabcf5b
Szymon <geding@geding-SVE14A1V1EB.(none)>
2013-03-12 16:38:13 +0100

    Init new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bash::Application.config.secret_token = 'e9a276ab019a4bcaf1bb586b5980ed563ff7fa2d2192ff8835f7d89d3bf880acaf8fb31c6ef090907f618a189ce2909f950656a342f904064f643c1ff36c2ddd'
